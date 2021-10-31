93bc21c552ad6cc8b3e82d87e0ad93235bfe6bac
Hugo Hiroshi <hugohst@gmail.com>
2021-10-31 17:33:33 -0300

    Update main.go

--- scraper/main.go
+++ scraper/main.go
@@ -7,12 +7,27 @@ import (
 	"os"
 	"strconv"
 	"sync"
+
 	"github.com/gocolly/colly"
 )
 
 func scrapPage(i int, wg *sync.WaitGroup, c *colly.Collector) {
 	fmt.Printf("Scraping Page: %d\n", i)
 	c.Visit("https://www.metacritic.com/browse/movies/score/metascore/all/filtered/netflix?page=" + strconv.Itoa(i))
+	wg.Done()
+}
+
+func scrapPage2(i int, wg *sync.WaitGroup, c2 *colly.Collector) {
+	fmt.Printf("Scraping Page: %d\n", i)
+	c2.Visit("https://www.imdb.com/search/title/?count=100&groups=top_1000&sort=user_rating" + strconv.Itoa(i))
+
+	wg.Done()
+}
+
+func scrapPage3(i int, wg *sync.WaitGroup, c3 *colly.Collector) {
+	fmt.Printf("Scraping Page: %d\n", i)
+	c3.Visit("https://www.rottentomatoes.com/browse/dvd-streaming-all" + strconv.Itoa(i))
+
 	wg.Done()
 }
 
@@ -25,11 +40,41 @@ func main() {
 	}
 	defer file.Close()
 
+	fName2 := "data2.csv"
+	file2, err := os.Create(fName2)
+	if err != nil {
+		log.Fatalf("Erro ao criar arquivo, err: %q", err)
+		return
+	}
+	defer file2.Close()
+
+	fName3 := "data3.csv"
+	file3, err := os.Create(fName3)
+	if err != nil {
+		log.Fatalf("Erro ao criar arquivo, err: %q", err)
+		return
+	}
+	defer file3.Close()
+
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
+	writer2 := csv.NewWriter(file2)
+	defer writer2.Flush()
+
+	writer3 := csv.NewWriter(file3)
+	defer writer3.Flush()
+
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.metacritic.com"),
+	)
+
+	c2 := colly.NewCollector(
+		colly.AllowedDomains("www.imdb.com"),
+	)
+
+	c3 := colly.NewCollector(
+		colly.AllowedDomains("www.rottentomatoes.com"),
 	)
 
 	c.OnHTML(".clamp-summary-wrap", func(e *colly.HTMLElement) {
@@ -37,7 +82,27 @@ func main() {
 			//e.ChildText("a"),
 			e.ChildText("h3"),
 			e.ChildText("span"),
-			e.ChildText("summary"),
+			//e.ChildText("text"),
+		})
+	})
+
+	c2.OnHTML(".lister-item-content", func(e *colly.HTMLElement) {
+		writer2.Write([]string{
+			//e.ChildText("a"),
+			e.ChildText("h3"),
+			e.ChildText("p"),
+			//e.ChildText("div"),
+		})
+	})
+
+	c3.OnHTML(".mb_info", func(e *colly.HTMLElement) {
+		writer3.Write([]string{
+			//e.ChildText("a"),
+
+			e.ChildText("a"),
+			e.ChildText("span"),
+			//e.ChildText("consensus"),
+			//e.ChildText("div"),
 		})
 	})
 
@@ -46,11 +111,33 @@ func main() {
 
 	for i := 0; i < 11; i++ {
 		go scrapPage(i, &waitGroup, c)
+
+		//go scrapPage2(i, &waitGroup, c2)
 		// agora vai ser em paralelo
 	}
 
 	waitGroup.Wait()
 
-	log.Printf("Scraping Completo")
+	var waitGroup2 sync.WaitGroup
+	waitGroup2.Add(11)
+	for i := 0; i < 11; i++ {
+
+		go scrapPage2(i, &waitGroup2, c2)
+
+	}
+	waitGroup2.Wait()
+
+	var waitGroup3 sync.WaitGroup
+	waitGroup3.Add(11)
+	for i := 0; i < 11; i++ {
+
+		go scrapPage2(i, &waitGroup3, c3)
+
+	}
+	waitGroup3.Wait()
+
+	log.Printf("Scraping Completo 13")
 	log.Println(c)
+	log.Println(c2)
+	log.Println(c3)
 }
