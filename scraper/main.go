d6ca5007e81807b258aa711be58a88d35a4176b3
Samuel James <bsbcraftplays@gmail.com>
2021-11-02 20:49:31 -0300

    Reorganizando projeto, falta ainda configurar a integração

--- scraper/main.go
+++ scraper/main.go
@@ -1,16 +1,93 @@
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
-	"sync"
-
-	"github.com/gocolly/colly"
+    _"encoding/csv"
+    "fmt"
+    _"log"
+    _"os"
+    _"strconv"
+    "strings"
+    "sync"
+    "bytes"
+    "github.com/gocolly/colly"
 )
+
+type scrap_context struct {
+	colly_meta 	    *colly.Collector
+    colly_imdb 		*colly.Collector
+    colly_rotten    *colly.Collector
+    colly_letter    *colly.Collector
+    response        *bytes.Buffer
+}
+
+// Lista de variáveis globais
+
+
+func criarCollys() scrap_context{
+    return scrap_context{
+        colly_meta: colly.NewCollector(
+            colly.AllowedDomains("www.metacritic.com"),
+        ),
+        colly_imdb: colly.NewCollector(
+            colly.AllowedDomains("www.imdb.com"),
+        ),
+        colly_rotten: colly.NewCollector(
+            colly.AllowedDomains("www.rottentomatoes.com"),
+        ),
+        colly_letter: colly.NewCollector(
+            colly.AllowedDomains("letterboxd.com"),
+        ),
+        response: bytes.NewBufferString(""),
+    }
+} 
+
+func configurarCollys(context *scrap_context) {
+    (*context).colly_meta.OnHTML(".clamp-summary-wrap", func(e *colly.HTMLElement) {
+        fmt.Fprint(context.response, "Resposta meta:")
+        /*writer.Write([]string{
+            e.ChildText("h3"),
+            e.ChildText("span"),
+        })*/
+    })
+
+    (*context).colly_imdb.OnHTML(".lister-item-content", func(e *colly.HTMLElement) {
+        fmt.Fprint(context.response, "Resposta IMDB:")
+        /*writer2.Write([]string{
+            e.ChildText("h3"),
+            e.ChildText("p"),
+        })*/
+    })
+
+    (*context).colly_meta.OnHTML(".main_stats", func(e *colly.HTMLElement) {
+        fmt.Fprint(context.response, "Resposta META2:")
+        /*writer3.Write([]string{
+            e.ChildText(".metascore_w"),
+            e.ChildText("a"),
+        })*/
+    })
+
+    (*context).colly_rotten.OnHTML("ul search-page-media-row", func(e *colly.HTMLElement) {
+        fmt.Fprint(context.response, "Resposta rotten:")
+        /*writer4.Write([]string{
+            e.ChildAttr("score-icon-critic", "percentage"),
+            e.ChildText("a"),
+        })*/
+    })
+
+    (*context).colly_letter.OnHTML(".film-detail-content", func(e *colly.HTMLElement) {
+        fmt.Fprint(context.response, "Resposta letter:")
+        /*writer5.Write([]string{
+            e.ChildText("a href"),
+            e.ChildText(".film-title-wrapper"),
+        })*/
+    })
+
+}
+
+func realizarPesquisaIndividual(wait_group *sync.WaitGroup, colly_atual* colly.Collector, url_pesquisa string) {
+    colly_atual.Visit(url_pesquisa)
+    wait_group.Done()
+}
 
 func scrapPage(i int, wg *sync.WaitGroup, c *colly.Collector) {
 	fmt.Printf("Scraping Page: %d\n", i)
@@ -21,196 +98,49 @@ func scrapPage(i int, wg *sync.WaitGroup, c *colly.Collector) {
 func scrapPage2(i int, wg *sync.WaitGroup, c2 *colly.Collector) {
 	fmt.Printf("Scraping Page: %d\n", i)
 	c2.Visit("https://www.imdb.com/search/title/?count=100&groups=top_1000&sort=user_rating" + strconv.Itoa(i))
-
-	wg.Done()
+    wg.Done()
 }
 
-//func scrapPage3(i int, wg *sync.WaitGroup, c3 *colly.Collector) {
-//	fmt.Printf("Scraping Page: %d\n", i)
-//	c3.Visit("https://www.rottentomatoes.com/browse/dvd-streaming-all" + strconv.Itoa(i))
+func realizarPesquisaParalelo(termo_pesquisa string, context *scrap_context) {
+    var wait_group sync.WaitGroup
+    
+    var termo_pesquisa_adaptada = strings.ReplaceAll(termo_pesquisa, " ", "+")
+    var termo_pesquisa_adaptada2 = strings.ReplaceAll(termo_pesquisa, " ", "%20")
+    
+    var pesquisa_meta = "https://www.metacritic.com/search/movie/" + termo_pesquisa_adaptada2 + "/results"
+    var pesquisa_rotten = "https://www.rottentomatoes.com/search?search=" + termo_pesquisa_adaptada2
+    var pesquisa_letter = "https://letterboxd.com/search/films/" + termo_pesquisa_adaptada + "/"
+    
+    // Colocamos 3 tasks para aguardar.
+    wait_group.Add(3)
+    go realizarPesquisaIndividual(&wait_group, context.colly_meta, pesquisa_meta)
+    go realizarPesquisaIndividual(&wait_group, context.colly_rotten, pesquisa_rotten)
+    go realizarPesquisaIndividual(&wait_group, context.colly_letter, pesquisa_letter)
+    
+    // Aguardamos as 3 tasks serem completas.
+    wait_group.Wait()
+}
 
-//	wg.Done()
-//}
-
-func main() {
-	fName := "data.csv"
-	file, err := os.Create(fName)
-	//var rows [][]string
-	var termo_pesquisa = "dune"
-	//var pesquisa = "https://www.metacritic.com/search/all/" + termo_pesquisa + "/results"
-	termo_pesquisa_adaptada := strings.ReplaceAll(termo_pesquisa, " ", "+")
-	termo_pesquisa_adaptada2 := strings.ReplaceAll(termo_pesquisa, " ", "%20")
-	var pesquisa_meta = "https://www.metacritic.com/search/movie/" + termo_pesquisa_adaptada2 + "/results"
-	var pesquisa_rotten = "https://www.rottentomatoes.com/search?search=" + termo_pesquisa_adaptada2
-
-	pesquisa_letter := "https://letterboxd.com/search/films/" + termo_pesquisa_adaptada + "/"
-
-	if err != nil {
-		log.Fatalf("Erro ao criar arquivo, err: %q", err)
-		return
-	}
-	defer file.Close()
-
-	fName2 := "data2.csv"
-	file2, err := os.Create(fName2)
-	if err != nil {
-		log.Fatalf("Erro ao criar arquivo, err: %q", err)
-		return
-	}
-	defer file2.Close()
-
-	fName3 := "data3.csv"
-	file3, err := os.Create(fName3)
-	if err != nil {
-		log.Fatalf("Erro ao criar arquivo, err: %q", err)
-		return
-	}
-	defer file3.Close()
-
-	fName4 := "data4.csv"
-	file4, err := os.Create(fName4)
-	if err != nil {
-		log.Fatalf("Erro ao criar arquivo, err: %q", err)
-		return
-	}
-	defer file4.Close()
-
-	fName5 := "data5.csv"
-	file5, err := os.Create(fName5)
-	if err != nil {
-		log.Fatalf("Erro ao criar arquivo, err: %q", err)
-		return
-	}
-	defer file5.Close()
-
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	writer2 := csv.NewWriter(file2)
-	defer writer2.Flush()
-
-	writer3 := csv.NewWriter(file3)
-	defer writer3.Flush()
-
-	writer4 := csv.NewWriter(file4)
-	defer writer4.Flush()
-
-	writer5 := csv.NewWriter(file5)
-	defer writer5.Flush()
-
-	c := colly.NewCollector(
-		colly.AllowedDomains("www.metacritic.com"),
-	)
-
-	c2 := colly.NewCollector(
-		colly.AllowedDomains("www.imdb.com"),
-	)
-
-	c3 := colly.NewCollector(
-		//colly.AllowedDomains("www.rottentomatoes.com"),
-		colly.AllowedDomains("www.metacritic.com"),
-	)
-
-	c4 := colly.NewCollector(
-		//colly.AllowedDomains("www.rottentomatoes.com"),
-		colly.AllowedDomains("www.rottentomatoes.com"),
-	)
-
-	c5 := colly.NewCollector(
-		//colly.AllowedDomains("www.rottentomatoes.com"),
-		colly.AllowedDomains("letterboxd.com"),
-	)
-
-	c.OnHTML(".clamp-summary-wrap", func(e *colly.HTMLElement) {
-		writer.Write([]string{
-			//e.ChildText("a"),
-			e.ChildText("h3"),
-			e.ChildText("span"),
-			//e.ChildText("text"),
-		})
-	})
-
-	c2.OnHTML(".lister-item-content", func(e *colly.HTMLElement) {
-		writer2.Write([]string{
-			//e.ChildText("a"),
-			e.ChildText("h3"),
-			e.ChildText("p"),
-			//e.ChildText("div"),
-		})
-	})
-
-	//c3.OnHTML(".mb_info", func(e *colly.HTMLElement) {
-	//	writer3.Write([]string{
-	//e.ChildText("a"),
-
-	//		e.ChildText("a"),
-	//		e.ChildText("span"),
-	//e.ChildText("consensus"),
-	//e.ChildText("div"),
-	//	})
-	//})
-	c3.OnHTML(".main_stats", func(e *colly.HTMLElement) {
-		writer3.Write([]string{
-			e.ChildText(".metascore_w"),
-			e.ChildText("a"),
-			//fmt.Println(string(e.Text))
-			//rows = append(rows, []string{string(titulo), string(nota)})
-		})
-	})
-	// Rotten html
-	c4.OnHTML("ul search-page-media-row", func(e *colly.HTMLElement) {
-		writer4.Write([]string{
-			e.ChildAttr("score-icon-critic", "percentage"),
-			e.ChildText("a"),
-		})
-
-	})
-
-	c5.OnHTML(".film-detail-content", func(e *colly.HTMLElement) {
-		writer5.Write([]string{
-			e.ChildText("a href"),
-			e.ChildText(".film-title-wrapper"),
-		})
-
-	})
-
-	var waitGroup sync.WaitGroup
-	waitGroup.Add(11)
+func montarDatabase() {
+    var wait_group sync.WaitGroup
+    wait_group.Add(11)
 
 	for i := 0; i < 11; i++ {
 		go scrapPage(i, &waitGroup, c)
-
-		//go scrapPage2(i, &waitGroup, c2)
-		// agora vai ser em paralelo
 	}
 
-	waitGroup.Wait()
-
-	var waitGroup2 sync.WaitGroup
-	waitGroup2.Add(11)
+	wait_group.Add(11)
 	for i := 0; i < 11; i++ {
-
 		go scrapPage2(i, &waitGroup2, c2)
-
 	}
-	waitGroup2.Wait()
 
-	//var waitGroup3 sync.WaitGroup
-	//waitGroup3.Add(11)
-	//for i := 0; i < 11; i++ {
+    // Aguardamos as tasks serem completas.
+    wait_group.Wait()
+}
 
-	//	go scrapPage2(i, &waitGroup3, c3)
-
-	//}
-	//waitGroup3.Wait()
-	log.Printf("Scraping " + termo_pesquisa)
-	c3.Visit(pesquisa_meta)
-	c4.Visit(pesquisa_rotten)
-	c5.Visit(pesquisa_letter)
-
-	log.Printf("Scraping Completo")
-	log.Println(c)
-	log.Println(c2)
-	log.Println(c3)
-	log.Println(c4)
+func main() {
+    var context = criarCollys()
+    configurarCollys(&context)
+    realizarPesquisaParalelo("Titanic", &context)
+    println(context.response.String())
 }
