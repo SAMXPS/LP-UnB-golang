3114cfda8f3fb19f287d02f8dd7cdec9fa4c1b6c
Samuel James <bsbcraftplays@gmail.com>
2021-11-02 22:44:58 -0300

    Continuando refatoração do backend

--- scraper/main.go
+++ scraper/main.go
@@ -1,30 +1,45 @@
 package main
 
 import (
-    _"encoding/csv"
-    "fmt"
-    _"log"
-    _"os"
-    _"strconv"
-    "strings"
-    "sync"
-    "bytes"
-    "github.com/gocolly/colly"
+    "encoding/csv"                  // Utilizado para construir banco CSV
+    "fmt"                           // Utilizado para formatação de strings  
+    "log"                           // Utilizado para log de mensagens 
+    "os"                            // Utilizado para abrir e escrever em arquivos
+    "strconv"                       // Utilizado para converter tipos (ex: int) em strings
+    "strings"                       // Utilizado para textos em strings
+    "sync"                          // Utilizado para paralelismo  
+    "github.com/gocolly/colly"      // Utilizado para fazer scraping das páginas  
 )
 
+// Struct de contexto para scraping
 type scrap_context struct {
-	colly_meta 	    *colly.Collector
-    colly_imdb 		*colly.Collector
-    colly_rotten    *colly.Collector
-    colly_letter    *colly.Collector
-    response        *bytes.Buffer
+	colly_meta 	    *colly.Collector    // Scraper do website metacritc 
+    colly_imdb 		*colly.Collector    // Scraper do website IMBD
+    colly_rotten    *colly.Collector    // Scraper do website rottentomatoes
+    colly_letter    *colly.Collector    // Scraper do website letterboxd
+    files           [5]*os.File         // Arquivos temporários CSV
+    writers         [5]*csv.Writer      // Writes do CSV
 }
 
-// Lista de variáveis globais
+// Função que cria contexto de execução do scraping
+func criarContexto() (*scrap_context, error){
+    var writers [5]*csv.Writer
+    var files [5]*os.File
 
+    // Abrimos 5 arquivos e 5 writers
+    for i := 0; i < 5; i++ {
+        fName := "data" + strconv.Itoa(i) + ".csv"
+        file, err := os.Create(fName)
+        if err != nil {
+            log.Fatalf("Erro ao criar arquivo, err: %q", err)
+            return nil, err
+        }
+        writers[i] = csv.NewWriter(file)
+        files[i] = file
+	}
 
-func criarCollys() scrap_context{
-    return scrap_context{
+    // Inicializamos os scrapers
+    context := scrap_context{
         colly_meta: colly.NewCollector(
             colly.AllowedDomains("www.metacritic.com"),
         ),
@@ -37,73 +52,76 @@ func criarCollys() scrap_context{
         colly_letter: colly.NewCollector(
             colly.AllowedDomains("letterboxd.com"),
         ),
-        response: bytes.NewBufferString(""),
+        writers: writers,
+        files: files,
     }
+    return &context, nil;
 } 
 
-func configurarCollys(context *scrap_context) {
+// Função que configura o contexto de scraping
+func configurarContexto(context *scrap_context) {
     (*context).colly_meta.OnHTML(".clamp-summary-wrap", func(e *colly.HTMLElement) {
-        fmt.Fprint(context.response, "Resposta meta:")
-        /*writer.Write([]string{
+        (*context).writers[0].Write([]string{
             e.ChildText("h3"),
             e.ChildText("span"),
-        })*/
+        })
     })
 
     (*context).colly_imdb.OnHTML(".lister-item-content", func(e *colly.HTMLElement) {
-        fmt.Fprint(context.response, "Resposta IMDB:")
-        /*writer2.Write([]string{
+        //fmt.Fprint(context.response, "Resposta IMDB:")
+        (*context).writers[1].Write([]string{
             e.ChildText("h3"),
             e.ChildText("p"),
-        })*/
+        })
     })
 
     (*context).colly_meta.OnHTML(".main_stats", func(e *colly.HTMLElement) {
-        fmt.Fprint(context.response, "Resposta META2:")
-        /*writer3.Write([]string{
+        //fmt.Fprint(context.response, "Resposta META2:")
+        (*context).writers[2].Write([]string{
             e.ChildText(".metascore_w"),
             e.ChildText("a"),
-        })*/
+        })
     })
 
     (*context).colly_rotten.OnHTML("ul search-page-media-row", func(e *colly.HTMLElement) {
-        fmt.Fprint(context.response, "Resposta rotten:")
-        /*writer4.Write([]string{
+        //fmt.Fprint(context.response, "Resposta rotten:")
+        (*context).writers[3].Write([]string{
             e.ChildAttr("score-icon-critic", "percentage"),
             e.ChildText("a"),
-        })*/
+        })
     })
 
     (*context).colly_letter.OnHTML(".film-detail-content", func(e *colly.HTMLElement) {
-        fmt.Fprint(context.response, "Resposta letter:")
-        /*writer5.Write([]string{
+        //fmt.Fprint(context.response, "Resposta letter:")
+        (*context).writers[4].Write([]string{
             e.ChildText("a href"),
             e.ChildText(".film-title-wrapper"),
-        })*/
+        })
     })
-
 }
 
-func realizarPesquisaIndividual(wait_group *sync.WaitGroup, colly_atual* colly.Collector, url_pesquisa string) {
-    colly_atual.Visit(url_pesquisa)
-    wait_group.Done()
-}
-
-func scrapPage(i int, wg *sync.WaitGroup, c *colly.Collector) {
+// Função que faz scraping de página do Metacritic
+func realizarScrapMetacritic(i int, wg *sync.WaitGroup, c *colly.Collector) {
 	fmt.Printf("Scraping Page: %d\n", i)
 	c.Visit("https://www.metacritic.com/browse/movies/score/metascore/all/filtered/netflix?page=" + strconv.Itoa(i))
 	wg.Done()
 }
 
-func scrapPage2(i int, wg *sync.WaitGroup, c2 *colly.Collector) {
+// Funçao que faz scraping de página do IMDB
+func realizarScrapImdb(i int, wg *sync.WaitGroup, c2 *colly.Collector) {
 	fmt.Printf("Scraping Page: %d\n", i)
 	c2.Visit("https://www.imdb.com/search/title/?count=100&groups=top_1000&sort=user_rating" + strconv.Itoa(i))
     wg.Done()
 }
 
-func realizarPesquisaParalelo(termo_pesquisa string, context *scrap_context) {
-    var wait_group sync.WaitGroup
-    
+// Função que realiza pesquisa na url e colly selecionados
+func realizarPesquisaIndividual(wait_group *sync.WaitGroup, colly_atual* colly.Collector, url_pesquisa string) {
+    colly_atual.Visit(url_pesquisa)
+    wait_group.Done()
+}
+
+// Função que pesquisa um filme em 3 sites diferentes, de forma paralela
+func realizarPesquisaParalelo(termo_pesquisa string, context *scrap_context, wait_group *sync.WaitGroup) {
     var termo_pesquisa_adaptada = strings.ReplaceAll(termo_pesquisa, " ", "+")
     var termo_pesquisa_adaptada2 = strings.ReplaceAll(termo_pesquisa, " ", "%20")
     
@@ -116,31 +134,43 @@ func realizarPesquisaParalelo(termo_pesquisa string, context *scrap_context) {
     go realizarPesquisaIndividual(&wait_group, context.colly_meta, pesquisa_meta)
     go realizarPesquisaIndividual(&wait_group, context.colly_rotten, pesquisa_rotten)
     go realizarPesquisaIndividual(&wait_group, context.colly_letter, pesquisa_letter)
-    
-    // Aguardamos as 3 tasks serem completas.
-    wait_group.Wait()
 }
 
-func montarDatabase() {
-    var wait_group sync.WaitGroup
-    wait_group.Add(11)
-
+// Função que faz scraping e monta banco de dados de melhores filmes
+func montarDatabase(context *scrap_context, wait_group *sync.WaitGroup) {
 	for i := 0; i < 11; i++ {
-		go scrapPage(i, &waitGroup, c)
+        wait_group.Add(2)
+		go realizarScrapMetacritic(i, &wait_group, context.colly_meta)
+		go realizarScrapImdb(i, &waitGroup, context.colly_imdb)
 	}
+}
 
-	wait_group.Add(11)
-	for i := 0; i < 11; i++ {
-		go scrapPage2(i, &waitGroup2, c2)
-	}
-
-    // Aguardamos as tasks serem completas.
-    wait_group.Wait()
+// Função para fechar writers e arquivos
+func encerrarContext(context *scrap_context) {
+    for i := 0; i < 5; i++ {
+        context.writers[i].Flush()
+        context.files[i].Close()
+    }
 }
 
 func main() {
-    var context = criarCollys()
-    configurarCollys(&context)
-    realizarPesquisaParalelo("Titanic", &context)
-    println(context.response.String())
+    context, err := criarContexto()
+    if err != nil {
+        return 
+    } else {
+        var wait_group sync.WaitGroup
+
+        // Criamos o contexto de scraping
+        configurarContexto(context)
+
+        // Rodamos as tarefas pendentes
+        go realizarPesquisaParalelo("Titanic", context, &wait_group)
+        go montarDatabase(context, &wait_group)
+
+        // Esperamos todas as tarefas finalizarem
+        wait_group.Wait()
+
+        // Encerramos o contexto de scraping
+        encerrarContext(context)
+    }
 }
